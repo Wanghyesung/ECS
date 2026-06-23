61e7e2d6fd4e6f0b2c1482b9feaafb643f341c92
Wanghyesung <awd5142@naver.com>
2026-06-24 08:49:14 +0900

    [Add] 보스 몬스터 에셋 추가
    -보스 몬스터 에셋 추가

---
+++ Assets/03_Monster/SOMonsterInfo.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_MonsterInfo", menuName = "Game/Monster Info")]
+public class SOMonsterInfo : ScriptableObject
+{
+    [Header("Identity")]
+    public string MonsterName = "New Monster";
+
+    [TextArea]
+    public string Description;
+    
+    [Header("Stats")]
+    public int MaxHP = 10;
+    public float Speed = 3f;
+    public float AttackRange = 1.5f;
+
+
+    [Header("Attack")]
+    public SOAttackInfo AttackInfo = null;
+}
+
+
+public enum eEntityState
+{
+    None,
+    Idle,
+    Move,
+    Attack,
+    Hit,
+    Dead,
+    End,
+}
+
